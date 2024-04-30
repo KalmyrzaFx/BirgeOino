fd9ef4a9e4182e00da9a04fe67236575022b16b6
Kalmyrza <zholchubekovk@gmail.com>
2024-04-30 19:18:37 +0600

    changing json

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
+// App.js
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utilis/setAuthToken';
-import { setCurrentUser, logoutUser  } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import { Container } from '@material-ui/core';
 import store from './store';
 
-import { 
+import {
   PrivateRoute,
   Navbar,
   Footer,
@@ -21,18 +23,16 @@ import {
   Profile,
   CreateProfile,
   UserProfile,
-  PrivacyPolicy,
-  TermsofService
- } from './components';
+} from './components';
 
-if(localStorage.jwtToken){
+if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
   const decoded = jwt_decode(localStorage.jwtToken);
   store.dispatch(setCurrentUser(decoded));
-  
+
   const currentTime = Date.now() / 1000;
-  
-  if(decoded.exp < currentTime){
+
+  if (decoded.exp < currentTime) {
     store.dispatch(logoutUser());
     window.location.href = '/login';
   }
