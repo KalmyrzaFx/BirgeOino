46a68dd4d152b7bdd165c9074743afcb20252855
Kalmyrza <zholchubekovk@gmail.com>
2024-05-09 10:43:56 +0600

    commti

--- client/src/axios-lists.js
+++ client/src/axios-lists.js
@@ -2,9 +2,14 @@ import axios from 'axios';
 
 let serverURL = 'http://localhost:8081';
 
+if(process.env.NODE_ENV === 'production'){
+   serverURL = 'https://birgeoino.onrender.com/';
+}
+
 const instance = axios.create({
    baseURL: serverURL
 });
+
 
 instance.defaults.headers.common['Authorization'] = `${localStorage.getItem('jwtToken')}`;
 export default instance;
