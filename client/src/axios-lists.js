edd82316b298910cb09bd19e21bbd56155ac4ccf
KalmyrzaFx <80804799+KalmyrzaFx@users.noreply.github.com>
2024-06-10 12:51:43 +0600

    Update axios-lists.js

--- client/src/axios-lists.js
+++ client/src/axios-lists.js
@@ -2,14 +2,20 @@ import axios from 'axios';
 
 let serverURL = 'http://localhost:8081';
 
-if(process.env.NODE_ENV === 'production'){
-   serverURL = 'https://birgeoino.onrender.com/';
+if (process.env.NODE_ENV === 'production') {
+    serverURL = 'https://birgeoino.onrender.com/';
 }
 
 const instance = axios.create({
-   baseURL: serverURL
+    baseURL: serverURL
 });
 
+// Check if there is a JWT token in localStorage
+const jwtToken = localStorage.getItem('jwtToken');
 
-instance.defaults.headers.common['Authorization'] = `${localStorage.getItem('jwtToken')}`;
+// If a JWT token exists, set it in the Authorization header
+if (jwtToken) {
+    instance.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
+}
+
 export default instance;
